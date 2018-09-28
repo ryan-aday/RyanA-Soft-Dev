d010592b100ed67a6f50f744644d4e3ea516b612
ryan-aday <raday@stuy.edu>
2018-09-28 04:46:10 -0400

    Main reads input, played around with requests

--- 13_HTML_Shenanigans/app.py
+++ 13_HTML_Shenanigans/app.py
@@ -1,20 +1,27 @@
+Ryan Aday
+SoftDev1 pd7
+K13 -- Echo
+2018-09-28 
+
 from flask import Flask, render_template, request
-import cgi
+
 app = Flask(__name__)#Creates an instance of Flask
 
 @app.route('/')
 def reroute():
-        return render_template("template.html")
+        return render_template("main.html")
+
 
 @app.route('/auth')
 def auth():
         #print (app)
         #print (request)
         #print (request.headers)
-        print (request.method)
+        ##print (request.method)
         #print (request.args)
         #print (request.form)
-        return "Whomst"
+        return render_template("auth.html", username=request.args["username"],
+                                method=request.method)
         
 if (__name__) == "__main__":#if this file is run directly then the Flask app will run
     app.debug = True#allows changes to directly affect local host without rerunning app
