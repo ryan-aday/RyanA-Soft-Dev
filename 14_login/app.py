13046e8a8b30c6de8a4597502e47c5745541c163
ryan-aday <raday@stuy.edu>
2018-10-01 21:01:52 -0400

    Fixing more bugs with git

---
+++ 14_login/app.py
@@ -0,0 +1,47 @@
+# Tina Wong, Ryan Aday
+# Team TL;DR
+# SoftDev1 pd7
+# K14 -- Do I Know You?
+# 2018-10-02
+
+from flask import Flask, render_template, request, session, redirect, url_for
+import os
+
+app = Flask(__name__) #create instance of class Flask
+app.secret_key = os.urandom(32)  #creates random key of 32 bytes
+
+users = {'test' : '123'}  #key and dict
+
+# home page that links to the form
+@app.route("/") #assign fxn to route
+def home():
+    # print("testing")
+    if 'test' not in session:
+        return render_template('login.html')
+    else:
+        user = 'test'
+        return render_template("welcome.html",user=user)
+
+# a route that receives the form and returns a template with user's information
+@app.route("/auth", methods=["POST"]) #assign fxn to route
+def authenticate():
+    if request.form['username'] not in users.keys():
+        return render_template("login.html", error = "Username invalid, is not "+request.form['username'])
+        #Returns login with Username error
+    
+    elif users[request.form['username']] != request.form['password']:
+        return render_template('login.html', error = "Password invalid, is not "+request.form['password'])
+        #Returns login with Password error
+    
+    else:
+        session['test'] = '123'
+        return redirect(url_for('home'))
+
+@app.route('/logout')
+def logout():
+    session.pop('test')  #Ends session
+    return redirect(url_for('home'))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
