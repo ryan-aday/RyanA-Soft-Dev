710eeefdcb0bef0709d452a567a60ce510b0d0eb
Ryan Aday <ryanobelisk@gmail.com>
2018-10-05 04:25:52 -0400

    Made code more modular by making reader a fxn, added comments for how to access code, moved data to data folder, modified table_name var to better read filename

--- 16_SQLite/app.py
+++ 16_SQLite/app.py
@@ -5,25 +5,30 @@
 
 import csv, sqlite3    
 
-filename="peeps.csv"  #Can be canged for any file to be read
-DB_FILE="discobandit.db"  #Creates .db file
+#filename="peeps.csv"  #Can be changed for any file to be read
+DB_FILE="app.db"  #Creates .db file
 
-db=sqlite3.connect(DB_FILE)
+db=sqlite3.connect(DB_FILE) #Open file if exists, otherwise make db file
 c= db.cursor()  #Manipulates rows in a query
 
-with open(filename) as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=",")  #Reads the csv file
-    header = reader.fieldnames  #Returns list of headers
-    table_name = filename.split(".")[0]  
+def csv_to_db(filename):
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=",")  #Reads the csv file
+        header = reader.fieldnames  #Returns list of headers
+        table_name = filename.split(".")[0].split("/")[-1]
 
-    query = "CREATE TABLE " + table_name + "(" + ",".join([col + " TEXT" for col in header]) + ")"
-    c.execute(query)
-    #Creates table via the sqlite command, join fxn joins all of the file names with type for roster
-
-    for row in reader:
-        query = "INSERT INTO " + table_name + " VALUES ("  + ",".join(["\"" + val + "\"" for val in row.values()]) + ")"
+        query = "CREATE TABLE " + table_name + "(" + ",".join([col + " TEXT" for col in header]) + ")"
         c.execute(query)
-        #Creates each entry into table, join fxn joins all of the key data with type for roster
+        #Creates table via the sqlite command, join fxn joins all of the file names with type for roster
+
+        for row in reader:
+            query = "INSERT INTO " + table_name + " VALUES ("  + ",".join(["\"" + val + "\"" for val in row.values()]) + ")"
+            c.execute(query)
+            #Creates each entry into table, join fxn joins all of the key data with type for roster
+
+csv_to_db("data/courses.csv")
+csv_to_db("data/peeps.csv")
+#Runs fnx for data files already in folder            
 
 db.commit()
 db.close()
