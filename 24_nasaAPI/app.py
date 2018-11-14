ad08c4150e25649ccdd1d1992fbfe37aaf84b9dd
Ryan Aday <ryanobelisk@gmail.com>
2018-11-13 23:37:08 -0500

    added apod

--- 24_nasaAPI/app.py
+++ 24_nasaAPI/app.py
@@ -1,13 +1,21 @@
 import urllib.request
-from flask import Flask, render_template, request
+import json
+from flask import Flask, render_template
+from util import parse_API
 
 app = Flask(__name__)#Creates an instance of Flask
 
 @app.route('/')
 def reroute():
-        u = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=31YHWQTWUpAhvwiZvXegOzVSmQ6u084ploE74SxV')
-        html = u.read()
-        return render_template("index.html", img="")
+        url='https://api.nasa.gov/planetary/apod?api_key=31YHWQTWUpAhvwiZvXegOzVSmQ6u084ploE74SxV'
+        with urllib.request.urlopen(url) as testfile, open('dataset.json', 'w') as f:
+            f.write(testfile.read().decode())
+        title=parse_API.title()
+        video=parse_API.video()
+        explanation=parse_API.explanation()
+        version=parse_API.version()
+        media_type=parse_API.media_type()
+        return render_template("index.html", video=video, title=title, explanation=explanation, version=version, media_type=media_type)
 
 if (__name__) == "__main__":#if this file is run directly then the Flask app will run
     app.debug = True#allows changes to directly affect local host without rerunning app
---
+++ 24_nasaAPI/util/parse_API.py
@@ -0,0 +1,39 @@
+import urllib.request
+import json
+
+def explanation():
+    data={}
+    with open("dataset.json", "r") as read_file:
+        data = json.load(read_file)
+    return data["explanation"]
+
+
+def video():
+    data={}
+    with open("dataset.json", "r") as read_file:
+        data = json.load(read_file)
+    return data["url"]
+
+def title():
+    data={}
+    with open("dataset.json", "r") as read_file:
+        data = json.load(read_file)
+    return data.get("title", "None")
+
+def version():
+    data={}
+    with open("dataset.json", "r") as read_file:
+        data = json.load(read_file)
+    return data.get("version", "None")
+
+def media_type():
+    data={}
+    with open("dataset.json", "r") as read_file:
+        data = json.load(read_file)
+    return data.get("media_type", "None")
+
+def imagesun():
+    data={}
+    with open("sundataset.json", "r") as read_file:
+        data = json.load(read_file)
+    return data.get("image", "None")
