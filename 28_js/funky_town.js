e0568c32fafdb1222b8b1370e1458c15658a9a9b
Ryan Aday <ryanobelisk@gmail.com>
2018-12-18 13:18:49 -0500

    converted functions to vars

--- 28_js/funky_town.js
+++ 28_js/funky_town.js
@@ -1,19 +1,24 @@
-function fact(n) {
+/* Team AK47 
+
+*/
+
+var factorial = (n) => {
     if(n == 0) {
         return 1
     }
     return n * fact(n - 1);
 }
 
-function fibonacci(n) {
+var fibonnaci = (n) => {
     if(n == 0) {
         return 0
     }
-    return n + fibonacci(n - 1);
+    if(n == 1)
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-function gcd(a, b){
-    if ( ! b) {
+var gcd = (a, b) =>{
+    if (!b) {
         return a;
     }
     return gcd(b, a % b);
@@ -21,6 +26,6 @@ function gcd(a, b){
 
 var students=["James", "Audrey", "TD", "DANK MEMER"]
 
-function randomStudent(){
+var randomStudent = () =>{
     return students[Math.floor(Math.random()*students.length)]; 
 }
