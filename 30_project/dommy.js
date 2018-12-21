d550d1ee31da1f5f7c647b9d86d5f824d474a3ff
Ryan Aday <ryanobelisk@gmail.com>
2018-12-21 12:07:28 -0500

    Fixed functions

--- 30_project/dommy.js
+++ 30_project/dommy.js
@@ -1,69 +1,43 @@
-var thelist= document.getElementById("thelist");
-
-var list0= document.getElementById("list0");
-var list1= document.getElementById("list1");
-var list2= document.getElementById("list2");
-var list3= document.getElementById("list3");
-var list4= document.getElementById("list4");
-var list5= document.getElementById("list5");
-var list6= document.getElementById("list6");
-var list7= document.getElementById("list7");
-
-var header = document.getElementById("h");
-var mainListC=8;
+/* Team AK47 
+Ryan Aday
+SoftDev1 pd<p>
+K30 -- Testing JS
+2018-12-21 
+*/
 
 /* Header Change */
-  mainList = thelist.getElementsByTagName('li');
-  console.log(mainList);
+var x = document.getElementById("thelist");
+var y = x.getElementsByTagName("li");
+var header = document.getElementById("h");
 
+for (i=0; i<y.length; i++){
+	let replacement=y[i];
+	replacement.addEventListener('mouseover', function(e){
+		header.innerHTML=replacement.innerHTML;
+	});
+}	
 
 /*Remove Main List Fxns */
-list0.addEventListener('click', function(e){
-	list0.remove();
-});
-
-list1.addEventListener('click', function(e){
-	list1.remove();
-});
-
-list2.addEventListener('click', function(e){
-	list2.remove();
-});
-
-list3.addEventListener('click', function(e){
-	list3.remove();
-});
-
-list4.addEventListener('click', function(e){
-	list4.remove();
-});
-
-list5.addEventListener('click', function(e){
-	list5.remove();
-});
-
-list6.addEventListener('click', function(e){
-	list6.remove();
-});
-
-list7.addEventListener('click', function(e){
-	list7.remove();
-});
-
+for (i=0; i<y.length; i++){
+	let replacement=y[i];
+	console.log(y[i]);
+	replacement.addEventListener('click', function(e){
+		replacement.remove();
+	});
+}
 
 /*Adding Elements to Main List*/
 
 var list = document.getElementById("thelist");
-
-
 var counter=8;
-var b=document.getElementById("b");
 
+var b=document.getElementById("b");
 b.addEventListener('click', function(e){
 	var li= document.createElement("li");
-	li.setAttribute("id", "list8");
-	li.appendChild(document.createTextNode("item New"));
-	list.appendChild(li);	
+	li.appendChild(document.createTextNode("item " + counter));
+	counter++;
+	list.appendChild(li);
+
 });
 
 
@@ -75,15 +49,22 @@ var fibonacci = (n) => {
     return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
-var counter=0;
+var fcounter=0;
 
 var fb=document.getElementById("fb");
 var fiblist=document.getElementById("fiblist");
 
 fb.addEventListener('click', function(e){
 var li= document.createElement("li");
-	li.appendChild(document.createTextNode(fibonacci(counter)));
-	counter++;
+	li.appendChild(document.createTextNode(fibonacci(fcounter)));
+	fcounter++;
 	fiblist.appendChild(li);	
+	let replacement=li;
+	replacement.addEventListener('click', function(e){
+		replacement.remove();
+	});
+	replacement.addEventListener('mouseover', function(e){
+		header.innerHTML=replacement.innerHTML;
+	});
 });
 
